b72184627bd903a2336ec2792a7d9193658c43d7
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-01-06 13:09:55 +1100

    DOC Updated config sample to match wiki

--- caches.conf.sample.php
+++ caches.conf.sample.php
@@ -1,39 +1,39 @@
 <?php
-
 SimpleCache::$cache_configs = array(
-	'default' => array(
-		'store_type' => 'SimpleFileBasedCacheStore',
-		'store_options' => array(
-			'silverstripe-cache/cache_store',
-		),
-		'cache_options' => array(
-			'expiry' => 3600
-		)
-	),
-	'publisher' => array(
-		'store_type' => 'SimpleFileBasedCacheStore',
-		'store_options' => array(
-			'silverstripe-cache/publisher',
-		),
-		'cache_options' => array(
-			'expiry' => 8640000			// a long time
-		)
-	)
+    'default' => array(
+        'store_type' => 'SimpleFileBasedCacheStore',
+        'store_options' => array(
+            'silverstripe-cache/cache_store',
+        ),
+        'cache_options' => array(
+            'expiry' => 3600
+        )
+    ),
+    'PublisherCache' => array(
+        'store_type' => 'SimpleFileBasedCacheStore',
+        'store_options' => array(
+            'publisher_cache',
+        ),
+        'cache_options' => array(
+            'expiry' => 8640000 
+        )
+    ),
+    'DynamicPublisherCache' => array(
+        'store_type' => 'SimpleFileBasedCacheStore',
+        'store_options' => array(
+            'silverstripe-cache/dynamic_publisher',
+        ),
+        'cache_options' => array(
+            'expiry' => 3600    
+        )
+    ),
+    'FragmentCache' => array(
+        'store_type' => 'SimpleFileBasedCacheStore',
+        'store_options' => array(
+            'silverstripe-cache/fragment_cache',
+        ),
+        'cache_options' => array(
+            'expiry' => 864000  
+        )
+    ),
 );
-
-/* 
- * A list of URLs (as regexes) that if matched, will have their content cached
- * for the given number of seconds
- */
-$cache_urls = array(
-	'custom-controller/view/\d+'      => 900,
-);
-
-/**
- * a list of hosts we're restricting access to 
- */
-$allowed_hosts = array(
-	'define.hosts.that.can.be.accessed.com',
-);
-
-include_once dirname(__FILE__).'/cache-main-auth.php';
