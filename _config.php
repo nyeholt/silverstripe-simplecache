305fd4b2e2e3af7eac9048e6d16a554b68047245
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-01-26 15:24:36 +1100

    Ensure proxy config file is loaded prior to referencing caches; fixes issue where CLI doesn't pick this up

--- _config.php
+++ _config.php
@@ -1,6 +1,9 @@
 <?php
 
 if (defined('USE_PROXY') && defined('PROXY_CONFIG_FILE')) {
+	if (!isset(SimpleCache::$cache_configs['PublisherCache'])) {
+		include_once BASE_PATH . '/' . PROXY_CONFIG_FILE;
+	}
 	foreach (array('PublisherCache', 'DynamicPublisherCache', 'FragmentCache') as $cacheName) {
 		$cache = SimpleCache::get_cache($cacheName);
 		if ($cache) {
