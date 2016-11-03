3081f406d168ae51e98f85ed88839081df0bd3e8
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-11-03 17:12:13 +1100

    FIX Ensure NeverCache option outputs the NoCache header value

--- code/extensions/SimpleCacheControllerExtension.php
+++ code/extensions/SimpleCacheControllerExtension.php
@@ -5,11 +5,11 @@
  */
 class SimpleCacheControllerExtension extends Extension
 {
-    public function onBeforeInit() {
+    public function onAftereInit() {
         if ($this->owner->NeverCache) {
             $res = $this->owner->getResponse();
             if ($res) {
-//                $res->addHeader('X-SilverStripe-NoCache', '1');
+                $res->addHeader('X-SilverStripe-NoCache', '1');
             }
         }
     }
